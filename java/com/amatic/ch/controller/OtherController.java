8da91e046cfe828d6f4f11be10d98a77e0f831cc
Jorge <Jorge@Jorge-PC>
2014-04-30 03:23:10 +0100

    +

---
+++ java/com/amatic/ch/controller/OtherController.java
@@ -0,0 +1,194 @@
+package com.amatic.ch.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.amatic.ch.constants.WebConstants;
+import com.amatic.ch.dto.Contacto;
+import com.amatic.ch.dto.Email;
+import com.amatic.ch.dto.User;
+import com.amatic.ch.service.PublicacionService;
+import com.amatic.ch.service.UserService;
+import com.amatic.ch.utils.WebUtils;
+import com.dyuproject.openid.OpenIdUser;
+
+@Controller
+public class OtherController {
+    private static final Logger log = LoggerFactory
+	    .getLogger(OtherController.class);
+    List<Integer> sessions = new ArrayList<Integer>();
+
+    @Autowired
+    private PublicacionService publicacionService;
+
+    @Autowired
+    private UserService userService;
+
+    @Value("#{application['domain']}")
+    String DOMAIN;
+
+    @Value("#{application['emailmail']}")
+    String EMAILMAIL;
+
+    @Value("#{application['emailcontact']}")
+    String EMAILCONTACT;
+
+    @RequestMapping(value = { "/contacto" }, method = { RequestMethod.GET,
+	    RequestMethod.POST })
+    public String getContacto(ModelMap model, HttpServletRequest request,
+	    HttpServletResponse response) throws IOException {
+
+	return "contacta";
+    }
+
+    @RequestMapping(value = { "/sobre" }, method = { RequestMethod.GET,
+	    RequestMethod.POST })
+    public String getSobre(ModelMap model, HttpServletRequest request,
+	    HttpServletResponse response) throws IOException {
+
+	return "sobre";
+    }
+
+    @RequestMapping(value = { "/politica-de-privacidad" }, method = {
+	    RequestMethod.GET, RequestMethod.POST })
+    public String getPoliticaPrivacidad(ModelMap model,
+	    HttpServletRequest request, HttpServletResponse response)
+	    throws IOException {
+
+	return "privacidad";
+    }
+
+    @RequestMapping(value = { "/suscribir" }, method = { RequestMethod.POST })
+    public void getSubscripcion(ModelMap model, HttpServletRequest request,
+	    @RequestParam("email") String email, HttpServletResponse response)
+	    throws IOException {
+	Email emailObj = new Email(email);
+	publicacionService.saveEmail(emailObj);
+	try {
+	    this.sendMail("From: " + email, "Nueva Suscripcion " + DOMAIN);
+	} catch (Exception e) {
+	    // TODO Auto-generated catch block
+	    log.error("Mail no mandado pero suscripcion realizada");
+	}
+	return;
+    }
+
+    @RequestMapping(value = { "/contactar" }, method = { RequestMethod.POST })
+    public void getContacto(ModelMap model, HttpServletRequest request,
+	    @RequestParam("email") String email,
+	    @RequestParam("name") String name,
+	    @RequestParam("message") String message,
+	    HttpServletResponse response) throws Exception {
+	Contacto contacto = new Contacto();
+	contacto.setEmail(email);
+	contacto.setMessage(message);
+	contacto.setName(name);
+	publicacionService.saveContacto(contacto);
+	this.sendMail("From: " + email + "\nName: " + name + "\nMessage:"
+		+ message, "Nueva consulta " + DOMAIN);
+	return;
+    }
+
+    @RequestMapping(value = { "/editar" }, method = { RequestMethod.GET,
+	    RequestMethod.POST })
+    public String getLogEditar(ModelMap model, HttpServletRequest request,
+	    HttpServletResponse response) throws IOException {
+	HttpSession session = request.getSession();
+
+	User user = (User) session
+		.getAttribute(WebConstants.SessionConstants.RC_USER);
+	if (user != null) {
+	    response.sendRedirect("/edicion/nuevo");
+	    return null;
+	} else {
+	    return "logEditar";
+	}
+	// //Ref<?> value has not been initialized
+	// if (user != null) {
+	// user = this.userService.findUser(user.getMail());
+	// }
+    }
+
+    @RequestMapping(value = "/preAuth", method = RequestMethod.GET)
+    public void getPreAuth(ModelMap model, HttpServletRequest request,
+	    HttpServletResponse response) throws IOException {
+
+	User userA = (User) request.getSession().getAttribute(
+		WebConstants.SessionConstants.RC_USER);
+	if (userA == null) {
+	    OpenIdUser userOId = (OpenIdUser) request.getSession()
+		    .getAttribute(OpenIdUser.ATTR_NAME);
+	    Map<String, Object> mUserOId = (Map<String, Object>) userOId
+		    .getAttributes().get("info");
+	    String mail;
+	    if (mUserOId.get("email") != null) {
+		mail = (String) mUserOId.get("email");
+	    } else {
+		mail = userOId.getIdentity();
+	    }
+	    try {
+		userA = this.userService.findUser(mail);
+		userA.setIpAddress(WebUtils.getClienAddress(request));
+		userA = this.userService.updateUserIp(userA);
+	    } catch (com.googlecode.objectify.NotFoundException nf) {
+		userA = new User();
+		userA.setMail(mail);
+		userA.setIpAddress(WebUtils.getClienAddress(request));
+		this.userService.create(userA, false);
+		User.setCont(0);
+		userA.setNewUser(true);
+	    }
+
+	    request.getSession().setAttribute(
+		    WebConstants.SessionConstants.RC_USER, userA);
+	}
+
+	response.sendRedirect("/edicion/nuevo");
+    }
+
+    static Properties props = new Properties();
+    static Session session = Session.getDefaultInstance(props, null);
+
+    public void sendMail(String msgBody, String subject) throws Exception {
+	try {
+	    log.warn("MAIL---------------: " + EMAILMAIL);
+	    log.warn("MAILCONTACT---------------: " + EMAILCONTACT);
+	    Message msg = new MimeMessage(session);
+	    msg.setFrom(new InternetAddress(EMAILMAIL, "Jorge " + DOMAIN));
+
+	    msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+		    EMAILCONTACT, "Jorge Amat"));
+
+	    msg.setSubject(subject);
+	    msg.setText(msgBody);
+	    Transport.send(msg);
+
+	} catch (Exception e) {
+	    log.error("error al mandar mail", e);
+	    throw e;
+	}
+    }
+
+}
---
+++ java/com/amatic/ch/controller/PublicacionAbstract.java
@@ -0,0 +1,329 @@
+package com.amatic.ch.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.akismet.Akismet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.ModelMap;
+
+import com.amatic.ch.constants.WebConstants;
+import com.amatic.ch.dto.Comentario;
+import com.amatic.ch.dto.Deref;
+import com.amatic.ch.dto.Publicacion;
+import com.amatic.ch.exception.UnknownResourceException;
+import com.amatic.ch.service.ComentarioService;
+import com.amatic.ch.service.PublicacionService;
+import com.amatic.ch.utils.WebUtils;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Ref;
+
+public abstract class PublicacionAbstract {
+    private static final Logger log = LoggerFactory
+	    .getLogger(PublicacionAbstract.class);
+    @Autowired
+    private PublicacionService publicacionService;
+    @Autowired
+    private ComentarioService comentarioService;
+
+    @Value("#{application['domain']}")
+    String DOMAIN;
+
+    @Value("#{application['brand']}")
+    String BRAND;
+
+    @Value("#{application['emailmail']}")
+    String EMAILMAIL;
+
+    @Value("#{application['emailcontact']}")
+    String EMAILCONTACT;
+
+    void guardarComentarioPub(HttpServletRequest request, String url,
+	    String nombre, String email, String puntos, String comentario,
+	    String web, String nbrComment, HttpServletResponse response)
+	    throws Exception {
+	Akismet akismet = new Akismet("49f8a3bfb431", "http://www." + DOMAIN);
+	boolean isSpam = akismet.commentCheck(request.getRemoteAddr(),
+		request.getHeader("User-agent"), request.getHeader("referer"),
+		"", // permalink
+		"comment", // comment type
+		"", // author
+		"", // email
+		"", comentario, // Text to check
+		request.getParameterMap());
+
+	if (isSpam) {
+	    // Mail.sendMail(
+	    // "Comentario Spam Akimet con ip "
+	    // + WebUtils.getClienAddress(request) + " y email: "
+	    // + email + "\n Dejado en:" + url + "\n Comentario:"
+	    // + comentario + "\n Web:" + web + "\n Puntos:"
+	    // + puntos + "\n Nombre:" + nombre,
+	    // "Spam Akimet comentario en " + BRAND);
+	    // response.sendRedirect("/");
+	    // response.flushBuffer();
+	    throw new UnknownResourceException("invalid comment");
+	} else {
+	    String key = WebUtils.SHA1(url.replaceAll("-", " "));
+	    Publicacion publicacion = publicacionService.getPublicacion(key,
+		    WebConstants.SessionConstants.EBOOK);
+	    if (publicacion == null) {
+		publicacion = publicacionService.getPublicacion(key,
+			WebConstants.SessionConstants.ARTICULO);
+	    }
+	    if (publicacion == null || Integer.parseInt(puntos) < 0
+		    || Integer.parseInt(puntos) > 5 || email == null
+		    || (email != null && email.trim().equals(""))) {
+		String uri = request.getRequestURI();
+		throw new UnknownResourceException("Error en comentario " + uri);
+		// return "channelNotFound";
+	    }
+
+	    List<Ref<Comentario>> lComentarios = publicacion.getlComentarios();
+	    Comentario nuevoComentario = new Comentario();
+	    if (!nbrComment.equals("") && Integer.parseInt(nbrComment) > 0) {
+		Ref<Comentario> refComentReply = lComentarios.get(Integer
+			.parseInt(nbrComment) - 1);
+		Comentario comentReply = Deref.deref(refComentReply);
+		nuevoComentario.setComentarioReply(comentReply.getComentario());
+		nuevoComentario.setComentarioReplyNombre(comentReply
+			.getNombre());
+		nuevoComentario.setComentarioReplyNbr(nbrComment);
+	    }
+
+	    nuevoComentario.setFecha(new Date());
+	    nuevoComentario.setMail(email);
+	    nuevoComentario.setNombre(nombre);
+	    nuevoComentario.setPuntos(Integer.parseInt(puntos));
+	    comentario = comentario.replaceAll("\r\n", "<br>");
+	    String safeComentario = Jsoup.clean(comentario, Whitelist.basic());
+	    safeComentario = safeComentario.replaceAll("\n", "");
+	    nuevoComentario.setComentario(safeComentario);
+	    nuevoComentario.setWeb(web);
+	    nuevoComentario.setGravatar(WebUtils.getGravatar80pxUrl(email));
+	    nuevoComentario.setIpAddress(WebUtils.getClienAddress(request));
+	    nuevoComentario.setPublicacion(publicacion);
+
+	    Key<Comentario> keyNuevoComentario = comentarioService
+		    .crearComentario(nuevoComentario);
+
+	    lComentarios.add(Ref.create(keyNuevoComentario));
+
+	    publicacionService.update(publicacion);
+	    this.sendMail(
+		    "Comentario con ip " + WebUtils.getClienAddress(request)
+			    + " y email: " + email + "\n Dejado en:" + url
+			    + "\n" + comentario + "\n Web:" + web
+			    + "\n Puntos:" + puntos + "\n Nombre:" + nombre,
+		    "Nuevo Comentario " + BRAND);
+	}
+
+    }
+
+    void setPublicaciones(ModelMap model, String tipo) {
+	List<Publicacion> publicaciones = publicacionService
+		.getPublicaciones(tipo);
+
+	List<String> categorias = new ArrayList<String>();
+	for (Publicacion publicacion : publicaciones) {
+	    if (!publicacion.getClase1().equals("")
+		    && !categorias.contains(publicacion.getClase1())) {
+		categorias.add(publicacion.getClase1());
+	    }
+	    if (!publicacion.getClase2().equals("")
+		    && !categorias.contains(publicacion.getClase2())) {
+		categorias.add(publicacion.getClase2());
+	    }
+	    if (publicacion.getClase7() != null
+		    && !publicacion.getClase7().equals("")
+		    && !categorias.contains(publicacion.getClase7())) {
+		categorias.add(publicacion.getClase7());
+	    }
+	    if (publicacion.getClase10() != null
+		    && !publicacion.getClase10().equals("")
+		    && !categorias.contains(publicacion.getClase10())) {
+		categorias.add(publicacion.getClase10());
+	    }
+	    if (publicacion.getClase11() != null
+		    && !publicacion.getClase11().equals("")
+		    && !categorias.contains(publicacion.getClase11())) {
+		categorias.add(publicacion.getClase11());
+	    }
+	    if (publicacion.getClase12() != null
+		    && !publicacion.getClase12().equals("")
+		    && !categorias.contains(publicacion.getClase12())) {
+		categorias.add(publicacion.getClase12());
+	    }
+	}
+	Collections.sort(categorias);
+
+	List<String> categoriasPrecio = new ArrayList<String>();
+	for (Publicacion publicacion : publicaciones) {
+	    if (!publicacion.getClase3().equals("")
+		    && !categoriasPrecio.contains(publicacion.getClase3())) {
+		categoriasPrecio.add(publicacion.getClase3());
+	    }
+	    if (!publicacion.getClase4().equals("")
+		    && !categoriasPrecio.contains(publicacion.getClase4())) {
+		categoriasPrecio.add(publicacion.getClase4());
+	    }
+
+	    if (!publicacion.getClase3().equals("")) {
+		publicacion.setClase5(publicacion.getClase1()
+			+ publicacion.getClase3());
+		if (!publicacion.getClase2().equals("")) {
+		    publicacion.setClase6(publicacion.getClase2()
+			    + publicacion.getClase3());
+		}
+		if (publicacion.getClase7() != null
+			&& !publicacion.getClase7().equals("")) {
+		    publicacion.setClase8(publicacion.getClase7()
+			    + publicacion.getClase3());
+		}
+		if (publicacion.getClase13() != null
+			&& !publicacion.getClase10().equals("")) {
+		    publicacion.setClase13(publicacion.getClase10()
+			    + publicacion.getClase3());
+		}
+		if (publicacion.getClase14() != null
+			&& !publicacion.getClase11().equals("")) {
+		    publicacion.setClase14(publicacion.getClase11()
+			    + publicacion.getClase3());
+		}
+		if (publicacion.getClase15() != null
+			&& !publicacion.getClase12().equals("")) {
+		    publicacion.setClase15(publicacion.getClase12()
+			    + publicacion.getClase3());
+		}
+	    }
+	}
+	Collections.sort(categoriasPrecio);
+
+	List<Publicacion> publicacionesMVE = publicacionService
+		.getPublicacionesMasVistas(WebConstants.SessionConstants.EBOOK);
+
+	List<Publicacion> publicacionesMVA = publicacionService
+		.getPublicacionesMasVistas(WebConstants.SessionConstants.ARTICULO);
+
+	List<Comentario> comentarios = comentarioService
+		.getUltimosComentarios();
+	List<Comentario> ultimosComentarios = new ArrayList<Comentario>();
+	for (Comentario comentario : comentarios) {
+	    Comentario ultimoComentario = new Comentario();
+	    ultimoComentario.setComentario(Jsoup.clean(
+		    comentario.getComentario(), Whitelist.simpleText()));
+	    ultimoComentario.setNombre(comentario.getNombre());
+	    ultimoComentario.setPublicacion(comentario.getPublicacion());
+	    ultimosComentarios.add(ultimoComentario);
+	}
+	model.addAttribute("comentarios", ultimosComentarios);
+	model.addAttribute("publicacionesMVE", publicacionesMVE);
+	model.addAttribute("publicacionesMVA", publicacionesMVA);
+	model.addAttribute("categorias", categorias);
+	model.addAttribute("categoriasPrecio", categoriasPrecio);
+	model.addAttribute("publicaciones", publicaciones);
+
+    }
+
+    String setPublicacion(String url, HttpServletRequest request, ModelMap model)
+	    throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	String key = WebUtils.SHA1(url.replaceAll("-", " "));
+	String view = "ebook";
+	Publicacion publicacion = publicacionService.getPublicacion(key,
+		WebConstants.SessionConstants.EBOOK);
+	if (publicacion == null) {
+	    publicacion = publicacionService.getPublicacion(key,
+		    WebConstants.SessionConstants.ARTICULO);
+	    view = "articulo";
+	}
+	if (publicacion == null) {
+	    String uri = request.getRequestURI();
+	    throw new UnknownResourceException("No existe el recurso: " + uri);
+	    // return "channelNotFound";
+	}
+	// incremeanting number viewers
+	publicacion.setNumVisitas(publicacion.getNumVisitas() + 1);
+	publicacionService.update(publicacion);
+
+	model.addAttribute("publicacion", publicacion);
+
+	List<Publicacion> publicaciones = publicacionService
+		.getUltimasPublicaciones(publicacion.getTipo());
+
+	List<Publicacion> publicacionesInteresantes = new ArrayList<Publicacion>();
+	for (Publicacion publicacionNoRep : publicaciones) {
+	    if (!publicacion.getKey().equals(publicacionNoRep.getKey())) {
+		publicacionesInteresantes.add(publicacionNoRep);
+	    }
+	}
+
+	List<Publicacion> publicacionesMVE = publicacionService
+		.getPublicacionesMasVistas(WebConstants.SessionConstants.EBOOK);
+
+	List<Publicacion> publicacionesMVA = publicacionService
+		.getPublicacionesMasVistas(WebConstants.SessionConstants.ARTICULO);
+
+	List<Comentario> comentarios = comentarioService
+		.getUltimosComentarios();
+	List<Comentario> ultimosComentarios = new ArrayList<Comentario>();
+	for (Comentario comentario : comentarios) {
+	    Comentario ultimoComentario = new Comentario();
+	    ultimoComentario.setComentario(Jsoup.clean(comentario
+		    .getComentario().replaceAll("<br />", " "), Whitelist
+		    .simpleText()));
+	    ultimoComentario.setNombre(comentario.getNombre());
+	    ultimoComentario.setPublicacion(comentario.getPublicacion());
+	    ultimosComentarios.add(ultimoComentario);
+	}
+	model.addAttribute("comentarios", ultimosComentarios);
+
+	model.addAttribute("publicacionesMVE", publicacionesMVE);
+
+	model.addAttribute("publicacionesMVA", publicacionesMVA);
+
+	model.addAttribute("publicaciones", publicacionesInteresantes);
+
+	return view;
+    }
+
+    static Properties props = new Properties();
+    static Session session = Session.getDefaultInstance(props, null);
+
+    public void sendMail(String msgBody, String subject) {
+	try {
+	    log.warn("MAIL---------------: " + EMAILMAIL);
+	    log.warn("MAILCONTACT---------------: " + EMAILCONTACT);
+	    Message msg = new MimeMessage(session);
+	    msg.setFrom(new InternetAddress(EMAILMAIL, "Jorge " + DOMAIN));
+
+	    msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+		    EMAILCONTACT, "Jorge Amat"));
+
+	    msg.setSubject(subject);
+	    msg.setText(msgBody);
+	    Transport.send(msg);
+
+	} catch (Exception e) {
+	    log.error("error al mandar mail", e);
+	}
+    }
+}
